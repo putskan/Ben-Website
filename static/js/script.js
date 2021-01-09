9adc6a5dc167b37f10b7fdfc5cb9ae38a5816eee
putskan <mule9003@gmail.com>
2021-01-09 15:57:18 +0200

    Add paperplane fly effect

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-
+let eventt 
 function fadeInEffect() {
 	let fadeElements = document.getElementsByClassName("fade-in-effect");
 	for (let e of fadeElements) {
@@ -11,6 +11,27 @@ function fadeInEffect() {
 }
 
 
+function flyAnimation(e) {
+	/* run flyAnimation on paper plane element 'e' */
+	console.log('flying');
+	e.style['animation-play-state'] = 'running';
+	setTimeout(function(){e.style['animation-play-state'] = 'paused';}, 3090);
+}
+
+
+function prepareFlyAnimation() {
+	let paperPlaneElements = document.getElementsByClassName("paper-plane");
+    for (let e of paperPlaneElements) {
+    	e.addEventListener("mouseover", function(event) {
+    		flyAnimation(e)
+    	});
+    }
+}
+
+
 window.addEventListener("load", function(){
-    fadeInEffect()
+    fadeInEffect();
+    prepareFlyAnimation();
 });
+
+
